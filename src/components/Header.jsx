628922877f904f7790c3a7926315a1259eab11ac
Константин Марьянкин <konstantinmarankin@MacBook-Pro-Konstantin.local>
2026-03-31 09:08:09 +0400

    redeploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,8 @@ function Header() {
     <header className={`header ${theme}`}>
       <div className="left">
         <div className="logo">
-        <img src={`${import.meta.env.BASE_URL}logo.png`} alt="logo" />
+        <img src={logo} alt="logo" className="header-logo" />
+
 
         </div>
 
