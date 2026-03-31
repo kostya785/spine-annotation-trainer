72dac5a96151b1c5660e5ed4167c8b35311bd563
Константин Марьянкин <konstantinmarankin@MacBook-Pro-Konstantin.local>
2026-03-31 08:49:11 +0400

    redeploy

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,22 +3,22 @@ import Lottie from "lottie-react";
 import spineAnimation from "../assets/MRI.json";
 import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
+
 function Main() {
   const { t } = useContext(AppContext);
+
   return (
     <section className="main-page">
       <div className="leftmain">
         <h1>{t.trust}</h1>
-        <p>
-          {t.anattools}
-        </p>
+        <p>{t.anattools}</p>
       </div>
 
       <div className="main-lottie-wrapper">
-        <Lottie 
-          animationData={spineAnimation} 
-          loop 
-          autoplay 
+        <Lottie
+          animationData={spineAnimation}
+          loop
+          autoplay
           className="main-lottie"
         />
       </div>
